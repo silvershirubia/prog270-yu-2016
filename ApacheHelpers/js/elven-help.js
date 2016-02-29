c7500867778866db85efa5318188707d7f7e5458
Sil at home <meh@example.com>
2016-02-29 00:55:55 -0800

    Midterm

--- ApacheHelpers/js/elven-help.js
+++ ApacheHelpers/js/elven-help.js
@@ -1,3 +1,22 @@
 $(document).ready(function() {
     $('table').addClass('table table-striped table-hover');
-});
+
+/*
+changeSize();
+    $(window).bind('resize', function(){
+	changeSize();
+}).trigger('resize');
+*/
+
+});//end ready
+
+/*
+function changeSize() {
+  if($(window).width() < 300){
+	$(".MTcontent").css("width", "100%");
+$(".MTimg").css("width", "100%");
+    });
+
+  }  
+};
+*/
